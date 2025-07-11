6342214dade56785833c16fd743d5c593a1786cc
Ivy Sham <ivysham888@gmail.com>
2025-07-11 22:08:58 +0800

    Initial Commit

---
+++ 250711_Birthday/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  250711_Birthday
+//
+//  Created by Ivy Sham on 11/7/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 250711_Birthday/_50711_BirthdayApp.swift
@@ -0,0 +1,17 @@
+//
+//  _50711_BirthdayApp.swift
+//  250711_Birthday
+//
+//  Created by Ivy Sham on 11/7/2025.
+//
+
+import SwiftUI
+
+@main
+struct _50711_BirthdayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
